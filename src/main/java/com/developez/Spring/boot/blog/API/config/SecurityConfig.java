4e2e6c5503d48f6325db9ff3471c91804cbc2865
Pochyxi <adienerlopez@gmail.com>
2023-09-28 19:46:12 +0200

    FINE SEZIONE JWT KEYCLOAK

--- src/main/java/com/developez/Spring/boot/blog/API/config/SecurityConfig.java
+++ src/main/java/com/developez/Spring/boot/blog/API/config/SecurityConfig.java
@@ -1,23 +1,18 @@
 package com.developez.Spring.boot.blog.API.config;
 
-import com.developez.Spring.boot.blog.API.Security.JwtAuthenticationEntryPoint;
-import com.developez.Spring.boot.blog.API.Security.JwtAuthenticationFilter;
+
 import com.developez.Spring.boot.blog.API.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
@@ -29,39 +24,22 @@ import java.util.List;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private UserDetailsService userDetailsService;
-
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    SecurityConfig( UserDetailsService userDetailsService,
-                    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-                    JwtAuthenticationFilter jwtAuthenticationFilter){
-        this.userDetailsService = userDetailsService;
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager( AuthenticationConfiguration configuration ) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public static PasswordEncoder passwordEncoder(){
-            return new BCryptPasswordEncoder();
-    }
 
     @Bean
     SecurityFilterChain securityFilterChain( HttpSecurity http ) throws Exception {
+        // CSRF TOKEN DISABILITATO
+//        // Crea un nuovo gestore di attributi di richiesta CSRF
+//        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+//        // Imposta il nome dell'attributo della richiesta CSRF a "_csrf"
+//        requestHandler.setCsrfRequestAttributeName( "_csrf" );
 
-        // Crea un nuovo gestore di attributi di richiesta CSRF
-        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
-        // Imposta il nome dell'attributo della richiesta CSRF a "_csrf"
-        requestHandler.setCsrfRequestAttributeName( "_csrf" );
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
 
         http
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+                )
                 // Configura CORS
                 .cors( ( httpSecurityCorsConfigurer ) -> httpSecurityCorsConfigurer
                         .configurationSource( ( httpServletRequest ) -> {
@@ -83,31 +61,24 @@ public class SecurityConfig {
                             return corsConfiguration;
                         } )
                 )
-                .csrf( ( httpSecurityCsrfConfigurer ) -> httpSecurityCsrfConfigurer
-                        // Imposta il gestore di attributi di richiesta CSRF
-                        .csrfTokenRequestHandler( requestHandler )
-                        // Ignora la protezione CSRF per i percorsi "/contact" e "/register"
-                        .ignoringRequestMatchers( "/api/auth/login", "/api/auth/register", "/api/auth/signin", "/api/auth/signup" )
-                        // Utilizza un repository di token CSRF basato su cookie con l'opzione HttpOnly disabilitata
-                        .csrfTokenRepository( CookieCsrfTokenRepository.withHttpOnlyFalse() )
-                )
+                // CSRF TOKEN DISABILITATO
+                .csrf().disable()
+                // CSRF TOKEN DISABILITATO
                 // Aggiunge il filtro CSRF personalizzato dopo il filtro di autenticazione di base
-                .addFilterAfter(
-                        new CsrfCookieFilter(), UsernamePasswordAuthenticationFilter.class
-                )
+//                .addFilterAfter(
+//                        new CsrfCookieFilter(), UsernamePasswordAuthenticationFilter.class
+//                )
                 .authorizeHttpRequests((authorize) -> {
 
                     authorize.requestMatchers( HttpMethod.GET, "/api/**").permitAll()
-//                            .requestMatchers( HttpMethod.GET, "/api/categories/**").permitAll()
                             .requestMatchers( "/api/auth/**" ).permitAll()
                             .anyRequest().authenticated();
-                }).exceptionHandling( exception -> exception
-                        .authenticationEntryPoint( jwtAuthenticationEntryPoint )
-                ).sessionManagement(session -> session
-                        .sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+                })// Configura la gestione del login tramite form oauth2
+                .oauth2ResourceServer( ( oauth2RS ) -> oauth2RS
+                        .jwt( ( jwt ) -> jwt
+                                .jwtAuthenticationConverter( jwtAuthenticationConverter )
+                        )
                 );
-
-        http.addFilterBefore( jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class );
 
         return http.build();
     }
